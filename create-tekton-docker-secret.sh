773e5ed3b6024d1c9fe21196b168a87280e7164a
Vincent-DeSousa-Tereso <Vincent.DeSousa.Tereso@ibm.com>
2019-05-13 11:11:53 -0400

    Fix var template to correspond

--- create-tekton-docker-secret.sh
+++ create-tekton-docker-secret.sh
@@ -7,11 +7,11 @@ kubectl create secret docker-registry registry-secret \
   --docker-server https://index.docker.io/v1/ \
   --docker-username ${DOCKER_USERNAME} \
   --docker-password ${DOCKER_PASSWORD} \
-  --namespace ${INSTALL_NAMESPACE}
+  --namespace ${DASHBOARD_INSTALL_NAMESPACE}
 
 kubectl patch secret registry-secret -p='{"metadata":{"annotations": {"tekton.dev/docker-0": "https://index.docker.io/v1/"}}}' \
---namespace ${INSTALL_NAMESPACE}
+--namespace ${DASHBOARD_INSTALL_NAMESPACE}
 
-kubectl patch sa ${DASHBOARD_SERVICE_ACCOUNT} -n ${INSTALL_NAMESPACE} \
+kubectl patch sa ${DASHBOARD_SERVICE_ACCOUNT} -n ${DASHBOARD_INSTALL_NAMESPACE} \
   --type=json \
   -p="[{\"op\":\"add\",\"path\":\"/secrets/0\",\"value\":{\"name\": \"registry-secret\"}}]"
